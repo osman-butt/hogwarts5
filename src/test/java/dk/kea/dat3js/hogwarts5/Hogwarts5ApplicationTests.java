82cc6218c4a804615801aa380d38f81e01effdd7
osman-butt <butt.osman@gmail.com>
2024-04-27 18:40:29 +0200

    Update Hogwarts5ApplicationTests.java

--- src/test/java/dk/kea/dat3js/hogwarts5/Hogwarts5ApplicationTests.java
+++ src/test/java/dk/kea/dat3js/hogwarts5/Hogwarts5ApplicationTests.java
@@ -2,8 +2,10 @@ package dk.kea.dat3js.hogwarts5;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class Hogwarts5ApplicationTests {
 
   @Test
