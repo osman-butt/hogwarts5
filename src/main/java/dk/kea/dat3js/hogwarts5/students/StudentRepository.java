04cd1c9c66b5f9f4b58cf8ece6149da176826bc6
osman-butt <butt.osman@gmail.com>
2024-04-28 02:29:38 +0200

    Added findByHouseName

--- src/main/java/dk/kea/dat3js/hogwarts5/students/StudentRepository.java
+++ src/main/java/dk/kea/dat3js/hogwarts5/students/StudentRepository.java
@@ -2,5 +2,8 @@ package dk.kea.dat3js.hogwarts5.students;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface StudentRepository extends JpaRepository<Student, Integer> {
+    List<Student> findByHouseName(String houseName);
 }
